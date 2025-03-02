8a3d4b5377e1b36303da3be52df28a6c894a266a
wsqstar <wsq_star@outlook.com>
2025-03-02 17:00:32 +0800

    Quartz sync: Mar 2, 2025, 5:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "zh-CN",
     baseUrl: "notes.gistudio.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
